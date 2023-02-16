1995fd815e8291f18f14eecc5d02a8d4682ac7ed
Sergii79 <tsymbal.sergiy@gmail.com>
2023-02-16 15:53:42 +0200

    commit second

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -5,3 +5,28 @@ console.log(galleryItems);
 
 console.log(_.add(4, 5));
 
+const galleryBlock = document.querySelector(".gallery");
+console.log(galleryBlock);
+
+
+
+const showGalleryItems = galleryItems
+    .map(({ preview, original, description }) => {
+        return `<li class="gallery__item" href="${original}">
+        <img class="gallery__image" src="${preview}" alt="${description}"></img></li>`
+    })
+    .join("");
+
+galleryBlock.insertAdjacentHTML("afterbegin", showGalleryItems);
+
+
+galleryBlock.addEventListener("click", onBigImg);
+
+
+// function onBigImg(event) {
+//     event.preventDefault();
+//     if (event.target.nodeName !== "IMG") {
+//         return;
+//     }
+// }
+// new SimpleLightbox(".gallery a");
