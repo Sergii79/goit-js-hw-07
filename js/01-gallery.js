451a35f42e13caa270d96c954eb824a1bcdfd217
Sergii79 <tsymbal.sergiy@gmail.com>
2023-02-17 19:52:44 +0200

    commit second

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,12 +1,12 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+// console.log(galleryItems);
 
-console.log(_.add(4, 5));
+// console.log(_.add(4, 5));
 
 const galleryBlock = document.querySelector(".gallery");
-console.log(galleryBlock);
+// console.log(galleryBlock);
 
 
 
@@ -20,13 +20,40 @@ const showGalleryItems = galleryItems
 galleryBlock.insertAdjacentHTML("afterbegin", showGalleryItems);
 
 
-galleryBlock.addEventListener("click", onBigImg);
+// galleryBlock.addEventListener("click", onBigImg);
+
+// const basicLightbox = require('basiclightbox')
+// import * as basicLightbox from 'basiclightbox'
+
+function onClick(event) {
+    event.preventDefault();
+    const imgs = event.target.classList.contains("gallery");
+    if (!imgs) {
+        return;
+    }
+}
+
+function onModal({
+    const instance = basicLightbox.create(
+        `<img src="${original}",
+        {
+            onShow: (instance) => {
+                galleryBlock.add
+            }
+        }`)
+    
+})
+
+function onClick(evt){
+    if (evt.target.nodeName !== `IMG`){
+        return;
+    }
+        const instance = basicLightbox.create(`
+        <img src="${evt.target.dataset.source}" width="800" height="600">
+        `)
+        instance.show();
+}
+  
 
 
-// function onBigImg(event) {
-//     event.preventDefault();
-//     if (event.target.nodeName !== "IMG") {
-//         return;
-//     }
-// }
-// new SimpleLightbox(".gallery a");
+
