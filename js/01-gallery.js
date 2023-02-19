7547db7a818cc68094ddfe64afcd1d87d0f9a9b3
Sergii79 <tsymbal.sergiy@gmail.com>
2023-02-19 14:46:10 +0200

    commit third

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,63 +1,50 @@
 import { galleryItems } from './gallery-items.js';
-// Change code below this line
-
-// console.log(galleryItems);
-
-// console.log(_.add(4, 5));
-
-const galleryBlock = document.querySelector(".gallery");
-// console.log(galleryBlock);
 
 
+const galleryBox = document.querySelector(".gallery");
 
+// Обробка галереї в html
 const showGalleryItems = galleryItems
-    .map(({ preview, original, description }) => {
-        return `<li class="gallery__item" 
-        href="${original}">
-        <img class="gallery__image" 
-        src="${preview}" 
-        alt="${description}">
-        </img></li>`
-    })
-    .join("");
+.map(({ preview, original, description }) => {
+    return `
+    <div class="gallery__item">
+    <a class="gallery__link" 
+    href="${original}">
+    <img class="gallery__image" 
+    src="${preview}" 
+    data-source="${original}"
+    alt="${description}">
+    </img>
+    </a>
+    </div>`
+})
+.join("");
 
-galleryBlock.insertAdjacentHTML("afterbegin", showGalleryItems);
+galleryBox.insertAdjacentHTML("afterbegin", showGalleryItems);
 
 
-// galleryBlock.addEventListener("click", onBigImg);
-
-// const basicLightbox = require('basiclightbox')
-// import * as basicLightbox from 'basiclightbox'
-
-// function onClick(event) {
-//     event.preventDefault();
-//     const imgs = event.target.classList.contains("gallery");
-//     if (!imgs) {
-//         return;
-//     }
-// }
-
-// function onModal({
-//     const instance = basicLightbox.create(
-//         `<img src="${original}",
-//         {
-//             onShow: (instance) => {
-//                 galleryBlock.add
-//             }
-//         }`)
+// Функція створення модального вікна
+function onGalleryContainerClick(event) {
     
-// })
+    event.preventDefault()
+    
+    const isImg = event.target.classList.contains('gallery__image');
+    if (!isImg) {
+        return
+    }
+    
+    const originalLink = event.target.dataset.source;    
+    
+    showBigImg(originalLink);
+}
 
-// function onClick(evt){
-//     if (evt.target.nodeName !== `IMG`){
-//         return;
-//     }
-//         const instance = basicLightbox.create(`
-//         <img src="${evt.target.dataset.source}" width="800" height="600">
-//         `)
-//         instance.show();
-// }
-  
+galleryBox.addEventListener('click', onGalleryContainerClick);
 
+// Посилання на відкриття модального вікна https://basiclightbox.electerious.com
+function showBigImg(link) {
+    const instance = basicLightbox.create(`
+    <img src="${link}">
+`)
 
-
+    instance.show()
+}
--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,31 @@
 import { galleryItems } from './gallery-items.js';
-// Change code below this line
+
 
 console.log(galleryItems);
+
+const galleryBox = document.querySelector(".gallery");
+
+// Обробка галереї в html
+const showGalleryItems = galleryItems
+.map(({ preview, original, description }) => {
+    return `
+   <a class="gallery__item" href="${original}">
+  <img class="gallery__image" src="${preview}" alt="${description}" />
+</a>`
+})
+.join("");
+
+galleryBox.insertAdjacentHTML("afterbegin", showGalleryItems);
+
+// Посилання на бібліотеку SimpleLightbox - JavaScript Options
+var lightbox = new SimpleLightbox('.gallery a', {    
+    captionsData: 'alt',
+    captionDelay: 250,
+});
+
+
+
+
+
+
+
